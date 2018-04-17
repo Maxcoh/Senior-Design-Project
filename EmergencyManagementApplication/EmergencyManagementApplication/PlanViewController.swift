170dac204a620312cd43b79bbcc3a55c9166daee
Maxcoh <blb98@drexel.edu>
2018-04-17 14:18:52 -0400

    Commit GUI On Screen Items

---
+++ EmergencyManagementApplication/EmergencyManagementApplication/PlanViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EmergencyManagementApplication
+//
+//  Created by Brendan Barnes on 3/6/18.
+//  Copyright © 2018 GloucesterCountyOEM. All rights reserved.
+//
+
+import UIKit
+
+class PlanViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
